87bb2982a7616d1a074b4d368931804eb8d27b9e
Krisha Garg <krishagarg18@gmail.com>
2025-06-23 00:14:42 +0530

    Initial commit - Full BlogVerse ASP.NET MVC project

---
+++ BlogVerse/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace BlogVerse.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // You can define your DB sets here, e.g.
+        // public DbSet<Blog> Blogs { get; set; }
+    }
+}
---
+++ BlogVerse/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogVerse.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int Views { get; set; }
+
+        public int Likes { get; set; }
+    }
+}
